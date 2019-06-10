322a94afb62583b7e1b2228a88c0aefcf07e6452
shivaacha <51356023+shivaacha@users.noreply.github.com>
2019-06-09 23:24:36 -0700

    Update sum.java

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+class sum { 
+public static void main(String args[])
+{
+int a, b, sum;
+System.out.println("enter two numbers:");
+Scanner in = new Scanner(System.in);
+sum=a+b;
+System.out.println("sum of two numbers is"+sum);
+}
+}
